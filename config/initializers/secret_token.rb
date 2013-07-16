2ae565e7298508f5f342b0d6901b5a8791d7af72
Dawenster <dstwen@gmail.com>
2013-07-16 10:45:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Totablets::Application.config.secret_token = 'ec1d6cb69c2258212f8975266bb4f392e00d4de690ddd9f214af9d369e2052b727ef63004b8b00c8cbc95bebb54fbdd89f73f42560f01615205d84f5d2bc60f7'
