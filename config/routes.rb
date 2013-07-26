8edaa8adae809876630c39cec142ef3482dc75a3
Dawenster <dstwen@gmail.com>
2013-07-25 22:14:08 -0700

    attempt non-www fix via routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Totablets::Application.routes.draw do
   post "/capture_customer_data" => "rentals#capture_customer_data", :as => :capture_customer_data
 
   mount Resque::Server.new, :at => "/resque"
+
+  constraints(:host => /totablets.com/) do
+    root :to => redirect("https://www.totablets.com")
+      match '/*path', :to => redirect {|params| "https://www.totablets.com/#{params[:path]}"}
+    end
+	end
 end
