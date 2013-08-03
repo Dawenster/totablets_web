ba5044c3c27ad2af6574eeb105c44441b83fc469
Dawenster <dstwen@gmail.com>
2013-08-02 17:54:25 -0700

    device controller methods implemented

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,46 @@
+class DevicesController < ApplicationController
+	http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
+
+	def index
+		@devices = Device.order("lower(name) ASC")
+	end
+
+	def new
+		@device = Device.new
+	end
+
+	def create
+		device = Device.new(params[:device])
+		if device.save
+			flash[:success] = "#{device.name} has been successfully created."
+			redirect_to devices_path
+		else
+			flash.now[:errors] = "Gawd. Fill everything in man."
+			@device = device
+			render "new"
+		end
+	end
+
+	def edit
+		@device = Device.find(params[:id])
+	end
+
+	def update
+		device = Device.find(params[:id])
+		device.assign_attributes(params[:device])
+		if device.save
+			flash[:success] = "#{device.name} has been updated."
+			redirect_to devices_path
+		else
+			flash.now[:errors] = "Gawd. Fill everything in man."
+			@device = device
+			render "edit"
+		end
+	end
+
+	def destroy
+		device = Device.find(params[:id]).destroy
+		flash[:success] = "#{device.name} has been deleted."
+		redirect_to devices_path
+	end
+end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,10 @@
 class Device < ActiveRecord::Base
-	attr_accessible :name, :profile_value, :device_type, :location
+	attr_accessible :name, :profile_value, :device_type, :location, :location_id
 
 	belongs_to :rentals
-	has_one :location
+	belongs_to :location
+
+	validates :name, :presence => true
+	validates :profile_value, :presence => true
+	validates :device_type, :presence => true
 end
