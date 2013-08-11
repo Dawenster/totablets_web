1954badfb13429e7f3835dc271d9aac2c71fb194
Dawenster <dstwen@gmail.com>
2013-08-10 17:20:51 -0700

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 gst = Tax.create(:name => "GST", :rate => 5)
-
 pst = Tax.create(:name => "PST", :rate => 7)
+hst = Tax.create(:name => "HST", :rate => 12)
 
-Location.create(
+nuvo = Location.create(
 	:name => "Nuvo Hotel",
 	:location_type => "Hotel",
 	:city => "Calgary",
 	:province_or_state => "Alberta",
-	:country => "Canada"
+	:country => "Canada",
 )
 
-Location.create(
+shangri_la = Location.create(
 	:name => "Shangri-La",
 	:location_type => "Hotel",
 	:city => "Vancouver",
@@ -18,8 +18,28 @@ Location.create(
 	:country => "Canada"
 )
 
-Device.create(
+1_king_west = Location.create(
+	:name => "1 King West",
+	:location_type => "Hotel",
+	:city => "Toronto",
+	:province_or_state => "Ontario",
+	:country => "Canada"
+)
+
+simulator = Device.create(
+	:name => "iPad Simulator",
+	:profile_value => "584342051651323254",
+	:device_type => "Simulator"
+)
+
+alpha = Device.create(
 	:name => "iPad Alpha",
-	:profile_value => "584342051651322821",
+	:profile_value => "584342051651323254",
 	:device_type => "iPad 2"
+)
+
+bravo = Device.create(
+	:name => "iPad Bravo",
+	:profile_value => "584342051651323755",
+	:device_type => "iPad 4"
 )
