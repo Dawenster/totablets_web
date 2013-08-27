d89f0722224bb23e350b946d9915076121c741ed
Dawenster <dstwen@gmail.com>
2013-08-26 23:43:09 -0700

    update website's contact info with info@totablets.com; make revenue split more ambiguous; available on the App Store tag for website; fix time stamp on admin access; added paginate to admin access

--- app/controllers/admin_accesses_controller.rb
+++ app/controllers/admin_accesses_controller.rb
@@ -2,6 +2,13 @@ class AdminAccessesController < ApplicationController
 	http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
 	
 	def index
-		@admin_accesses = AdminAccess.order("created_at DESC")
+		@admin_accesses = AdminAccess.order("created_at DESC").paginate(page: params[:page], :per_page => 25)
+		@time_differences = {
+			"PST" => 0,
+			"MST" => 1,
+			"CST" => 2,
+			"EST" => 3
+		}
+		Time.zone = "Pacific Time (US & Canada)"
 	end
 end
