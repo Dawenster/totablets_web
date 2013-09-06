884a3521b993accc395db2fed6e37defacb6d75f
Dawenster <dstwen@gmail.com>
2013-09-06 18:32:47 +0800

    attempting fix of rentals index route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Totablets::Application.routes.draw do
 
 	root to: 'pages#home'
 
-  resources :rentals, :only => [:create]
+  # resources :rentals, :only => [:create]
   post "/location_info" => "rentals#location_info", :as => :location_info
   post "/capture_customer_data" => "rentals#capture_customer_data", :as => :capture_customer_data
   post "/admin_command" => "rentals#admin_command", :as => :admin_command
@@ -12,7 +12,7 @@ Totablets::Application.routes.draw do
   scope '/admin' do
 	  get '/' => 'admins#index', :as => :admin_index
 	  resources :key_inputs, :only => [:show, :edit, :update]
-	  resources :rentals, :only => [:index, :show]
+	  resources :rentals, :only => [:index, :show, :create]
 	  resources :customers, :only => [:index, :show]
 	  resources :locations, :except => [:show]
 	  resources :devices, :except => [:show]
